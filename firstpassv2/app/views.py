65f5f3d91d4e184d06f0dca98f8e869d0789d476
NuzziVF <dylan.mc.cervantes7@gmail.com>
2022-10-31 09:07:48 -0500

    added delete function and changed to table

--- firstpassv2/app/views.py
+++ firstpassv2/app/views.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 from django.shortcuts import render, redirect
 from .forms import NewUserForm, NewBusinessForm
 from django.contrib.auth import login, authenticate, logout
@@ -35,15 +36,19 @@ def homepage(request):
 def get_data(request):
 
     my_data = Listing.objects.all()  # for all the records
-    one_data = Listing.objects.get(
-        pk=1
-    )  # 1 will return the first item change it depending on the data you want
-    context = {
-        "my_data": my_data,
-        "one_data": one_data,
-    }
+    # one_data = Listing.objects.get(
+    #     pk=1
+    # )  # 1 will return the first item change it depending on the data you want
+    my_lists = Listing.objects.all().values()
+    context = {"my_data": my_data, "my_lists": my_lists}
 
     return render(request, "get_data.html", context)
+
+
+def delete(request, id):
+    member = Listing.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect("/get_data/")
 
 
 def register_request(request):
