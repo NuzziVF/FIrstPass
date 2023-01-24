19f3fcff894135147d376b4fb8c5ed60d9bf9c76
NuzziVF <dylan.mc.cervantes7@gmail.com>
2023-01-24 09:11:46 -0600

    moved file location

--- djangofiles/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class password_model(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-
-class password_storage(models.Model):
-    stored_passwords = models.CharField(max_length=100)
